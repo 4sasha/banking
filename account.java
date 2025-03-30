14568669dbde7c4974a1ba1711d2c5d12967d790
4sasha <lambrsa@whs.school.nz>
2025-03-31 12:41:36 +1300

    Add files via upload

---
+++ account.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class account here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class account
+{
+   public account(String name, int number, int address, String type, int balance)
+   {
+       this.name = name;
+       this.number = number;
+       this.address = address;
+       this.type = type;
+       this.balance = balance;
+    }
+   
+   
+}
